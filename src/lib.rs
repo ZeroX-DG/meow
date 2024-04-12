9ea831a0364c58beaab4a9a7d52c0b658f233c58
ZeroX-DG <viethungax@gmail.com>
2024-04-13 08:11:44 +1200

    removed compiler until parser completed

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
-mod compiler;
 mod lexer;
 mod parser;
 mod span;
 mod stream;
 
-use compiler::Compiler;
 use lexer::Lexer;
 use parser::Parser;
 
 pub fn compile(input: &str) -> String {
     let tokens = Lexer::tokenize(input).expect("Error while tokenizing");
     let program = Parser::parse(tokens).expect("Error while parsing");
-    Compiler::compile(program).expect("Error while compiling")
+    format!("{:?}", program)
 }
